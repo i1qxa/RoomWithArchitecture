766f29a9fa46b0f8ca14a50ee0410b91b4273a63
i1qxa <kuiv1989@gmail.com>
2022-03-28 21:34:59 +0300

    заакончил создание БД

---
+++ app/src/main/java/com/example/roomwitharchitecture/Word.kt
@@ -0,0 +1,10 @@
+package com.example.roomwitharchitecture
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id:Int,
+    @ColumnInfo(name = "Word") val word:String)
